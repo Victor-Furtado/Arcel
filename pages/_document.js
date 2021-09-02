2d7b56f86489d1c912ad7ebca963a43c183945d6
stolen <victor.furtado@devpunk.page>
2021-09-02 09:14:00 -0300

    crossorigin fix

--- pages/_document.js
+++ pages/_document.js
@@ -24,7 +24,7 @@ export default class MyDocument extends Document {
             <Html>
                 <Head>
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
                     <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet" />
                     {this.props.styleTags}
                 </Head>
